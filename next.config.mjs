7d28c4a4e29adcaed20873bf1e0e0b59188d6927
rkhatta1 <rkhatta1@asu.edu>
2026-01-01 16:03:03 -0700

    [fix]: stashed the config changes.

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,16 @@
-import createMDX from '@next/mdx';
-
 /** @type {import('next').NextConfig} */
 
 const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig = {
-    output: 'export',  // Required for GitHub Pages static export
+    output: 'export',
     basePath: isDev ? '' : '/rkhatta1',
     assetPrefix: isDev ? '' : '/rkhatta1/',
     images: {
-      unoptimized: true, // For static export
+      unoptimized: true,
       domains: ['github.io'],
     },
     trailingSlash: true, 
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-};
-
-const withMDX = createMDX({
-  options: {},
-})
+  };
   
-export default withMDX(nextConfig);
+  export default nextConfig;
